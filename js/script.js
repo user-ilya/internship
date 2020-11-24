dca700a2bd36a1cacfef66c8e5750343891348d4
Ilya <ILYAkuzmin31072001@yandex.ru>
2020-11-24 22:55:10 +0300

    four__internship

---
+++ js/script.js
@@ -0,0 +1,44 @@
+const myName = prompt("Введите ваше имя", ""),
+      age =prompt("Введите ваш возраст", ""),
+      btn = document.querySelector(".button__main"),
+      textLorem = document.querySelector(".text");
+    
+
+alert(`Привет,${myName}!`);
+
+    textLorem.addEventListener("mouseover", () => {
+        if (age >= 18) {
+        console.log("Событие успешно произведено");
+        } else if (age < 18 ) {
+            alert("Вам еще нет 18 лет");
+        }
+        else {
+            alert("Error");
+        }
+    });
+    
+// конкатенация 
+console.log(typeof(+"string"));
+console.log(typeof(!!4));
+
+
+function sumNumbers (a,b) {
+    return a + b;
+}
+try {
+    btn.addEventListener("mouseover", () => {
+      console.log(sumNumbers(4,6));
+        
+    });
+} catch (error) {
+    console.log(error.name); 
+}
+
+let user = 0;
+try {
+    user = 5/d;
+} catch (error) {
+    console.log("Ошибка");
+}
+// экранирование 
+alert( "Глава 5.1".match(/\d\.\d/) );
